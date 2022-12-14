fb720a1d73c93019184b39e0ec9c3e910e8e3198
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2022-12-14 22:58:49 +0100

    add passport

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.set('strictQuery', false);
+
 mongoose.connect("mongodb://localhost:27017/userDB")
 
 const userSchema = new mongoose.Schema({
