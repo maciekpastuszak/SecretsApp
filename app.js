3379da1f6d706f09b9bebfa6a61a7b8bbd7329cc
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2022-12-17 23:04:09 +0100

    add passport configuration

--- app.js
+++ app.js
@@ -42,6 +42,19 @@ passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo'
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
 app.get("/", (req,res) => {
     res.render("home");
 });
